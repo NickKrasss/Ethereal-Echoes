d80563058a3f4fe31af905064d8760f09f3916ff
Алексей <aleks3204932@mail.ru>
2024-11-07 21:52:42 +0300

    Creating a resolution selection button and a post-effects button

--- EtherealEchoes/Assets/GameOptions.cs
+++ EtherealEchoes/Assets/GameOptions.cs
@@ -1,12 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class GameOptions : MonoBehaviour
 {
+    public TMP_Dropdown dropdown;
     //Фуллскрин
     public void FullScreen()
     {
         Screen.fullScreen = !Screen.fullScreen;
+    }
+    //Настройка разрешений
+    public void ChangeRelosution()
+    {
+        if (dropdown.value == 0)
+        {
+            Screen.SetResolution(1920, 1080,true);
+        }
+        else if (dropdown.value == 1)
+        {
+            Screen.SetResolution(1366, 768, true);
+        }
+        else if (dropdown.value == 2)
+        {
+            Screen.SetResolution(1280, 800, true);
+        }
+        else if (dropdown.value == 3)
+        {
+            Screen.SetResolution(1440, 900, true);
+        }
+        else if (dropdown.value == 4)
+        {
+            Screen.SetResolution(1280, 1024, true);
+        }
+        else if (dropdown.value == 5)
+        {
+            Screen.SetResolution(1600, 900, true);
+        }
+        else if (dropdown.value == 6)
+        {
+            Screen.SetResolution(2560, 1440, true);
+        }
+        else if (dropdown.value == 7)
+        {
+            Screen.SetResolution(3840, 2160, true);
+        }
     }
 }
