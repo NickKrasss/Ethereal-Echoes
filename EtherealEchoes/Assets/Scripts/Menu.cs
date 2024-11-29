3d1495a76a47b0f05c44b5abbc6c078ad78e6f07
Алексей <aleks3204932@mail.ru>
2024-11-29 19:49:07 +0300

    Исправление багов с фуллскрином, корректировка меню

--- EtherealEchoes/Assets/Scripts/Menu.cs
+++ EtherealEchoes/Assets/Scripts/Menu.cs
@@ -33,37 +33,38 @@ public class Menu : MonoBehaviour
     //Настройка разрешений
     public void ChangeRelosution()
     {
+
         if (dropdown.value == 0)
         {
-            Screen.SetResolution(1920, 1080, true);
+            Screen.SetResolution(1920, 1080, Screen.fullScreen);
         }
         else if (dropdown.value == 1)
         {
-            Screen.SetResolution(1366, 768, true);
+            Screen.SetResolution(1366, 768, Screen.fullScreen);
         }
         else if (dropdown.value == 2)
         {
-            Screen.SetResolution(1280, 800, true);
+            Screen.SetResolution(1280, 800, Screen.fullScreen);
         }
         else if (dropdown.value == 3)
         {
-            Screen.SetResolution(1440, 900, true);
+            Screen.SetResolution(1440, 900, Screen.fullScreen);
         }
         else if (dropdown.value == 4)
         {
-            Screen.SetResolution(1280, 1024, true);
+            Screen.SetResolution(1280, 1024, Screen.fullScreen);
         }
         else if (dropdown.value == 5)
         {
-            Screen.SetResolution(1600, 900, true);
+            Screen.SetResolution(1600, 900, Screen.fullScreen);
         }
         else if (dropdown.value == 6)
         {
-            Screen.SetResolution(2560, 1440, true);
+            Screen.SetResolution(2560, 1440, Screen.fullScreen);
         }
         else if (dropdown.value == 7)
         {
-            Screen.SetResolution(3840, 2160, true);
+            Screen.SetResolution(3840, 2160, Screen.fullScreen);
         }
     }
     public void StartGame()
