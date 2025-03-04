da567478b645d41301ba3b8d09bed8ba9d739042
Алексей <aleks3204932@mail.ru>
2025-03-04 22:18:43 +0300

    Добавил UI временный для шестерёнок

---
+++ EtherealEchoes/Assets/Scripts/Player/GearContainer.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+public class GearContainer : MonoBehaviour
+{
+    public int current_gears;
+    public int max_gears;
+    TMP_Text textMeshPro;  
+    public void AddGears(int gears)
+    {
+        if (current_gears  < max_gears)
+        {
+            current_gears += gears;
+            if (current_gears > max_gears)
+            {
+                current_gears = max_gears;
+            }
+        }
+    }
+    private void Update()
+    {
+        if (!textMeshPro)
+        {
+            textMeshPro = GameObject.FindGameObjectWithTag("Gears").GetComponent<TMP_Text>();
+            return;
+        }
+        textMeshPro.text = $"{current_gears} / {max_gears}";
+    }
+}
